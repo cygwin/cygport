19b257d4d202a1af8cfe889cf273f43a0c6b1de5
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2006-11-22 04:44:59 +0000

    Add HTML version of man page with man2html.

--- autogen.sh
+++ autogen.sh
@@ -5,24 +5,23 @@ set -e
 B=$(pwd)
 S=${0%/*}
 
-running() {
-	echo ">>> Running ${1}..."
-}
-
 export WANT_AUTOCONF=2.5
 export WANT_AUTOMAKE=1.10
 
+V=$(head -n 1 ${S}/configure.ac | cut -d , -f 2)
+
+verbose() {
+	echo ">>> Running ${@} ..."
+	${@}
+}
+
 cd ${S}
-running aclocal
-aclocal --force
-running autoconf
-autoconf --force
-running automake
-automake --add-missing --copy --force-missing
+verbose aclocal --force
+verbose autoconf --force
+verbose automake --add-missing --copy --force-missing
 
 cd ${B}
-running configure
-${S}/configure \
-	--srcdir=${S} --enable-maintainer-mode \
-	--prefix=/usr --datarootdir=/usr/share --sysconfdir=/etc \
+verbose ${S}/configure \
+	--srcdir=${S} --enable-maintainer-mode --prefix=/usr --sysconfdir=/etc \
+	--datarootdir=/usr/share --docdir=/usr/share/doc/cygport-${V// } \
 	"${@}"
