34ee5cfa08d3a571438115056f70fc1aef56ce69
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2007-03-14 20:14:15 +0000

    * bin/prep_scrollkeeper_omf.sh: Improve postinstall and add preremove.

--- bin/prep_scrollkeeper_omf.sh
+++ bin/prep_scrollkeeper_omf.sh
@@ -3,25 +3,37 @@
 #
 # prep_scrollkeeper_omf.sh - Postinstall commands for GNOME Yelp documentation
 # Part of cygport - Cygwin packaging application
-# Copyright (C) 2006 Yaakov Selkowitz
+# Copyright (C) 2006, 2007 Yaakov Selkowitz
 # Provided by the Cygwin Ports project <http://cygwinports.dotsrc.org/>
 # Distributed under the terms of the GNU General Public License v2
 #
-# $Id: prep_scrollkeeper_omf.sh,v 1.4 2006-11-20 05:48:58 yselkowitz Exp $
+# $Id: prep_scrollkeeper_omf.sh,v 1.5 2007-03-14 20:14:15 yselkowitz Exp $
 #
 ################################################################################
 set -e
 
-# FIXME: scrollkeeper-update should be run only for individual .omf dirs
-# FIXME: create a preremove script which updates the removal as well
+sk_vardir=/var/lib/scrollkeeper
+sk_omfdir=/usr/share/omf
 
-if [ -d ${D}/var/lib/scrollkeeper ]
+if [ -d ${D}${sk_vardir} ]
 then
-	rm -fr ${D}/var/lib/scrollkeeper
+	rm -fr ${D}${sk_vardir}
 fi
 
-dodir /etc/postinstall
-cat >> ${D}/etc/postinstall/${PN}.sh <<-_EOF
-	scrollkeeper-update -p /var/lib/scrollkeeper
-	
-	_EOF
+for d in ${D}${sk_omfdir}/*
+do
+	pkg_omfdir=${d#${D}}
+
+	dodir /etc/postinstall
+	cat >> ${D}/etc/postinstall/${PN}.sh <<-_EOF
+		scrollkeeper-update -q -o ${pkg_omfdir} -p ${sk_vardir}
+
+		_EOF
+
+	dodir /etc/preremove
+	cat >> ${D}/etc/preremove/${PN}.sh <<-_EOF
+		rm -fr ${pkg_omfdir}
+		scrollkeeper-update -q -o ${sk_omfdir} -p ${sk_vardir}
+
+		_EOF
+done
