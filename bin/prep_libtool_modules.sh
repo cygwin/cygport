50299add14988e1a8c1dff7c2811c0b4cfd38657
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2007-10-12 05:31:40 +0000

    * bin/prep_libtool_modules.sh: Check that library was properly installed. Simplify code and add comments.

--- bin/prep_libtool_modules.sh
+++ bin/prep_libtool_modules.sh
@@ -7,7 +7,7 @@
 # Provided by the Cygwin Ports project <http://cygwinports.dotsrc.org/>
 # Distributed under the terms of the GNU General Public License v2
 #
-# $Id: prep_libtool_modules.sh,v 1.9 2007-08-02 21:49:45 yselkowitz Exp $
+# $Id: prep_libtool_modules.sh,v 1.10 2007-10-12 05:31:40 yselkowitz Exp $
 #
 ################################################################################
 set -e
@@ -18,39 +18,55 @@ echo "Fixing libtool modules:"
 
 for lib_la
 do
+	# sanity check
 	if [ ! -f ${lib_la} ]
 	then
 		error "file ${lib_la} does not exist!"
 	fi
 
+	# check that the file is actually a libtool library
+	# e.g. xmodmap.la, where 'la' is a language code
 	if ! grep -q "libtool library file" ${lib_la}
 	then
-		continue    # go to next iteration of for loop
+		continue
 	fi
 
 	source ${lib_la}
+
+	ltlibdir=${lib_la%/*}
+
+	# check that all library members were installed
+	for l in dlname library_names old_library
+	do
+		if defined ${l} && [ ! -f ${ltlibdir}/${!l} ]
+		then
+			# FIXME: error?
+			warning "${!l} was not installed"
+		fi
+	done
 
 	# Only a -module should be "fixed" in this way
 	if [ "x${shouldnotlink}" = "xyes" ]
 	then
 		echo "        ${lib_la}"
 
-		declare ltlibdir=${lib_la%/*}
-		declare ltlibname=$(echo ${lib_la##*/} | sed -e 's:\.la$::')
+		ltlibname=${lib_la##*/}
+		ltlibname=${ltlibname%.la}
 
 		if [ "x${dlname}" = "x" ]
 		then
-			error "${lib_la} dynamic module was not built"
+			error "${ltlibname}.la dynamic module was not built"
 		fi
 
-		# warn if -avoid-version was not used with -module (which 99.9% of time should be)
-		if [ "${dlname#../bin/}" != "cyg${ltlibname#lib}.dll" \
-		     -a "${dlname#../bin/}" != "${ltlibname}.dll" \
-		     -a "${dlname}" != "${ltlibname}.so" ]
-		then
-			warning "${lib_la} appears to be a versioned module."
-		fi
+		# warn if -avoid-version was not used with -module
+		# 99.9% of time should be, but there are notable exceptions
+		case "${dlname#../bin/}" in
+			cyg${ltlibname#lib}.dll|${ltlibname}.dll)	;;
+			cyg${ltlibname#lib}.so|${ltlibname}.so)		;;
+			*)	warning "${ltlibname}.la appears to be a versioned module." ;;
+		esac
 
+		# static and import libraries are pointless for modules
 		rm -f ${ltlibdir}/${ltlibname}.a ${ltlibdir}/${ltlibname}.dll.a
 
 		cat > ${lib_la} <<-_EOF
@@ -93,6 +109,7 @@ do
 			libdir='${libdir}'
 			_EOF
 	else
+		# shouldnotlink = no; this is a regular library
 		true
-	fi	# shouldnotlink = yes #
+	fi
 done
