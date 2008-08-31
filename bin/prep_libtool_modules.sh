3bc6e4c795fac6dadd7ead026dc80f23afe1622d
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2008-08-31 04:52:30 +0000

    * bin/cygport.in: lib/berkdb.cygclass (berkdb_autoreconf): lib/cross.cygclass (cross_config_sub): lib/gnome2.cygclass (gnome2_autoreconf): lib/kde3.cygclass (kde3_autoreconf): Remove libtool 1.5 support.
    * bin/prep_libtool_modules.sh: Speed up libtool version detection.

--- bin/prep_libtool_modules.sh
+++ bin/prep_libtool_modules.sh
@@ -25,7 +25,7 @@
 ################################################################################
 set -e
 
-declare -r ltversion="$(/usr/bin/libtool --version | head -n 1)"
+declare -r ltversion="$(grep '^# ltmain.sh (GNU' /usr/bin/libtool | cut -d ' ' -f 2-)"
 
 echo "Fixing libtool modules:"
 
