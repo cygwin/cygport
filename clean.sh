1db74fe2b524ec8d3c34342d09b3d110691632ea
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2010-01-07 08:56:41 +0000

    Update.

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -fr autom4te.cache aclocal.m4 configure config.log config.status
-rm -f INSTALL install-sh missing
+rm -f ChangeLog INSTALL install-sh missing
 rm -f $(find ${0%/*} -name Makefile.in -o -name Makefile)
-rm -f bin/cygport doc/cygport.1 cygport-*.tar.*
+rm -f bin/cygport doc/cygport.1* doc/manual.* cygport-*.tar.*
 rm -fr tests/cygport* tests/foo*
