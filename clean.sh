c36a6a66bd818273f0a4d05b87a6af755f601b02
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2010-03-21 12:51:35 -0500

    Remove clean.sh script, use git clean instead

--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -fr autom4te.cache aclocal.m4 configure config.log config.status
-rm -f ChangeLog INSTALL install-sh missing
-rm -f $(find ${0%/*} -name Makefile.in -o -name Makefile)
-rm -f bin/cygport doc/cygport.1* doc/manual.* cygport-*.tar.*
-rm -fr tests/cygport* tests/foo*
